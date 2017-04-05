5f35b02af7dfcf4991616e9b08d2f79cf3453768
maomao <1263203465@qq.com>
2017-04-05 22:18:10 +0800

    登录提交

--- js/home/login.js
+++ js/home/login.js
@@ -1,3 +1,24 @@
-define([], function() {
-	
+define(['jquery'], function($) {
+	$('#login-form').on('submit', function() {
+    //这里的this是form原生对象,对form进行包装，才可以调用jq的方法
+    // console.log($(this).serialize());
+    // console.log($(this).serializeArray());
+
+    $.ajax({
+      url: '/v6/login',
+      type: 'post',
+      data: $(this).serialize(),
+      success: function(data) {
+        if(data.code === 200){
+          location.href = '/'
+        }
+      },
+      error: function(){
+        alert(arguments[2]);
+      }
+    });
+      //阻止表单默认提交
+    return false;
+  })
+
 });
